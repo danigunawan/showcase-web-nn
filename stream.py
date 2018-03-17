1720109bb1ef008607058f21c1458a1f94a76196
Ivar Matstoms <ivar.matstoms@gmail.com>
2018-03-17 13:09:46 +0100

    show image

--- stream.py
+++ stream.py
@@ -1,15 +1,28 @@
 from flask import Blueprint, render_template, request
 import numpy as np
+import cv2
 
 from server_config import config
+import style_transfer
 
 stream_app = Blueprint('stream_app', __name__)
 
 @stream_app.route("/<nn_type>/send", methods=["POST"])
 def index(nn_type):
     camera_size=(int(config["IMAGE"]["size_x"]), int(config["IMAGE"]["size_y"]))
-    print(np.array(list(request.data)).reshape([*camera_size,3]))
+    frame=np.array(list(request.data)).reshape([*camera_size,3]).astype(np.uint8)
+    gray=style_transfer.transform(frame)
+    global win
+    if not win:
+        #win=True
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            pass
+    
+    
     return nn_type
+
+win=False
 
 """
 def parse_camera_blob(blob):
